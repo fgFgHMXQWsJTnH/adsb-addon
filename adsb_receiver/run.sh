744e82e9ac41b6ff72a87acfa31f792c64acb746
fgFgHMXQWsJTnH <rocketslingshotdev@gmail.com>
2026-03-02 23:36:07 +0000

    Update run.sh

--- adsb_receiver/run.sh
+++ adsb_receiver/run.sh
@@ -6,7 +6,18 @@ LAT=$(bashio::config 'lat')
 LON=$(bashio::config 'lon')
 GAIN=$(bashio::config 'gain')
 
-bashio::log.info "Starting dump1090 on serial: ${SERIAL}"
+bashio::log.info "Looking for RTL-SDR device with serial: ${SERIAL}"
+
+# Find the device index for our serial number
+DEVICE_INDEX=$(rtl_test 2>&1 | grep -n "${SERIAL}" | head -1 | awk -F: '{print $1}')
+DEVICE_INDEX=$((DEVICE_INDEX - 1))
+
+if [ -z "${DEVICE_INDEX}" ]; then
+    bashio::log.error "Could not find device with serial ${SERIAL}"
+    exit 1
+fi
+
+bashio::log.info "Found device at index ${DEVICE_INDEX}, starting dump1090"
 
 if [ "${GAIN}" = "auto" ]; then
     GAIN_ARG="--gain -10"
@@ -15,7 +26,7 @@ else
 fi
 
 exec dump1090-mutability \
-    --device-index ":${SERIAL}" \
+    --device-index ${DEVICE_INDEX} \
     --lat "${LAT}" \
     --lon "${LON}" \
     ${GAIN_ARG} \
